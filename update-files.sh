0741f4ceb9696fbe16a18b98c0bc39de99ffa4c9
Camber Huang <camber@poi.science>
2025-09-14 13:20:30 +0800

    ci: clean script comments
    Signed-off-by: Camber Huang <camber@poi.science>

--- update-files.sh
+++ update-files.sh
@@ -15,18 +15,3 @@ while IFS= read -r file; do
     sha256sum $dir/$filename \
         > $dir/$filename.sha256sum
 done < files.list
-
-# echo '[+] Removing old files ...'
-# for i in `cat files.list`; do
-#     rm -v $(basename $i)
-# done
-# rm -v *.sha256sum
-# 
-# echo '[+] Downloading files ...'
-# wget -i files.list
-# 
-# echo '[+] Creating checksums (sha256) ...'
-# for i in `cat files.list`; do
-#     sha256sum $(basename $i) \
-#         > $(basename $i).sha256sum
-# done
